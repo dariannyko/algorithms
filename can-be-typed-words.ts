192a2f7f5be84afe866a11cc9560ae55ca04983e
d.ovsyannikova <d.ovsyannikova@bftcom.com>
2024-03-15 22:54:07 +0300

    refactor: https://leetcode.com/problems/maximum-number-of-words-you-can-type

--- can-be-typed-words.ts
+++ can-be-typed-words.ts
@@ -1,16 +1,16 @@
-function isBrokenWord(word: string, letters: string) {
-  for (let i = 0; i < letters.length; i++) {
-    if (word.includes(letters[i])) return true;
-  }
-  return false;
-}
-
 function canBeTypedWords(text: string, brokenLetters: string): number {
   const words = text.split(" ");
 
-  const normalWords = words.filter(
-    (item) => !isBrokenWord(item, brokenLetters)
-  );
+  const normalWords = words.filter((item) => {
+    let isBroken = false;
+    for (const letter of brokenLetters) {
+      if (item.includes(letter)) {
+        isBroken = true;
+        return;
+      }
+    }
+    return !isBroken;
+  });
 
   return normalWords.length;
 }
