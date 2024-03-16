0369d29c55c70f85f43b63fdfe0ae1ccf5ef17f4
d.ovsyannikova <d.ovsyannikova@bftcom.com>
2024-03-16 22:06:16 +0300

    https://leetcode.com/problems/count-operations-to-obtain-zero/

---
+++ count-operations.ts
@@ -0,0 +1,16 @@
+function countOperations(num1: number, num2: number): number {
+  let operations = 0;
+
+  while (num1 && num2) {
+    operations++;
+    if (num1 >= num2) {
+      num1 -= num2;
+    } else {
+      num2 -= num1;
+    }
+  }
+
+  return operations;
+}
+
+countOperations(2, 3);
