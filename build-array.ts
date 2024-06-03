64f7021a4e4e4332bbe726892db89fc75399439d
d.ovsyannikova <d.ovsyannikova@bftcom.com>
2024-06-04 00:58:56 +0300

    refactor

--- build-array.ts
+++ build-array.ts
@@ -1,8 +1,17 @@
 function buildArray(target: number[], n: number): string[] {
-  const stack: number[] = [];
+  const stack: string[] = [];
+  let index = 0;
 
-  
+  for (let i = 1; i <= n && index < target.length; i++) {
+    if (target[index] === i) {
+      stack.push("Push");
+      index++;
+    } else {
+      stack.push("Push", "Pop");
+    }
+  }
 
+  return stack;
 }
 
-buildArray([1, 3], 3);
+buildArray([1, 3], 3); // ["Push","Push","Pop","Push"]
