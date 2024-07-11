d323038d151c898643dedb55c0e5491488652b48
Daria Nnyko <dariannyko@gmail.com>
2024-07-11 16:28:57 +0300

    https://leetcode.com/problems/two-furthest-houses-with-different-colors (#135)

--- two-furthest-houses-with-different-colors.ts
+++ two-furthest-houses-with-different-colors.ts
@@ -1,4 +1,13 @@
-// todo
 function maxDistance(colors: number[]): number {
-  return 0;
+  let max = 0;
+
+  for (let i = 0; i < colors.length; i++) {
+    for (let j = i + 1; j < colors.length; j++) {
+      if (colors[i] !== colors[j]) max = Math.max(max, j - i);
+    }
+  }
+
+  return max;
 }
+
+maxDistance([1, 1, 1, 6, 1, 1, 1]); // 3
