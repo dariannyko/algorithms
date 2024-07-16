8aeebf2f8623ad4462a48f35a7dc3f5b9df990cd
dariannyko <dariannyko@gmail.com>
2024-07-16 17:08:19 +0300

    https://leetcode.com/problems/count-number-of-distinct-integers-after-reverse-operations

--- count-number-of-distinct-integers-after-reverse-operations.ts
+++ count-number-of-distinct-integers-after-reverse-operations.ts
@@ -1,3 +1,22 @@
+function reverseNumber(num: number) {
+  let res = 0;
+
+  while (num) {
+    res = res * 10 + (num % 10);
+    num = Math.floor(num / 10);
+  }
+
+  return res;
+}
+
 function countDistinctIntegers(nums: number[]): number {
-  // TODO  
-};
+  const set = new Set(nums);
+
+  for (let num of nums) {
+    set.add(reverseNumber(num));
+  }
+
+  return set.size;
+}
+
+countDistinctIntegers([1, 13, 10, 12, 31]); // 6
