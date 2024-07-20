f7813370d9049abecbfd80b122b83059c52318d9
Daria Nnyko <dariannyko@gmail.com>
2024-07-20 22:29:54 +0300

    https://leetcode.com/problems/minimum-distance-to-the-target-element (#143)
    * https://leetcode.com/problems/minimum-distance-to-the-target-element
    
    * refactor

---
+++ get-min-distance.ts
@@ -0,0 +1,15 @@
+function getMinDistance(nums: number[], target: number, start: number): number {
+  let right, left;
+
+  right = left = start;
+
+  while (true) {
+    if (nums[right] === target) return right - start;
+    if (nums[left] === target) return start - left;
+
+    left--;
+    right++;
+  }
+}
+
+getMinDistance([1, 2, 3, 4, 5], 5, 3); // 1
