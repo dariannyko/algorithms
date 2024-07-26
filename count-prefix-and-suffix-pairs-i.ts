0421e368c5ccbce6625a61250f2c29886cb6422d
d.ovsyannikova <dariannyko#gmail.com>
2024-07-26 20:43:43 +0300

    https://leetcode.com/problems/count-prefix-and-suffix-pairs-i

--- count-prefix-and-suffix-pairs-i.ts
+++ count-prefix-and-suffix-pairs-i.ts
@@ -1,9 +1,17 @@
-{
-  // todo
-  function countPrefixSuffixPairs(words: string[]): number {
-    console.log("!");
-    return 1;
+function isPrefixAndSuffix(str1: string, str2: string) {
+  return str2.startsWith(str1) && str2.endsWith(str1);
+}
+
+function countPrefixSuffixPairs(words: string[]): number {
+  let pairs = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    for (let j = i + 1; j < words.length; j++) {
+      if (isPrefixAndSuffix(words[i], words[j])) pairs++;
+    }
   }
 
-  console.log();
+  return pairs;
 }
+
+countPrefixSuffixPairs(["a", "aba", "ababa", "aa"]); // 4
