0f07ee9f9d1a2b275dc186069d0969b59a1ba640
d.ovsyannikova <dariannyko#gmail.com>
2024-07-28 17:24:47 +0300

    https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array

--- find-all-numbers-disappeared-in-an-array.ts
+++ find-all-numbers-disappeared-in-an-array.ts
@@ -1,3 +1,12 @@
 function findDisappearedNumbers(nums: number[]): number[] {
-  // TODO  
-};
+  const set = new Set(nums);
+  const res: number[] = [];
+
+  for (let i = 1; i <= nums.length; i++) {
+    if (!set.has(i)) res.push(i);
+  }
+
+  return res;
+}
+
+findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]); // [5, 6]
