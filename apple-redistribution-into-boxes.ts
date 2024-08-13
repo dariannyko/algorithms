21f27b1f48b9f895cb426afcbb84d94d8fa55d35
d.ovsyannikova <dariannyko#gmail.com>
2024-08-13 21:35:10 +0300

    https://leetcode.com/problems/apple-redistribution-into-boxes

---
+++ apple-redistribution-into-boxes.ts
@@ -0,0 +1,15 @@
+function minimumBoxes(apple: number[], capacity: number[]): number {
+  let sum = apple.reduce((sum, item) => sum + item, 0);
+  let minBoxes = 0;
+
+  capacity.sort((a, b) => b - a);
+
+  for (let i = 0; i < capacity.length && sum > 0; i++) {
+    sum -= capacity[i];
+    minBoxes++;
+  }
+
+  return minBoxes;
+}
+
+console.log(minimumBoxes([1, 3, 2], [4, 3, 1, 5, 2]));
