fca72c9163b393af61734626d4b5e5408f61539d
d.ovsyannikova <dariannyko#gmail.com>
2024-08-15 22:28:59 +0300

    https://leetcode.com/problems/minimum-number-of-operations-to-convert-time

--- minimum-number-of-operations-to-convert-time.ts
+++ minimum-number-of-operations-to-convert-time.ts
@@ -1,4 +1,28 @@
-// todo
+// TODO: refactor
 function convertTime(current: string, correct: string): number {
-    return 0
+    let counter = 0;
+
+    const cur = getMinutes(current);
+    const cor = getMinutes(correct);
+    let rest = Math.abs(cur - cor);
+
+    if (Math.floor(rest / 60) > 0) {
+        counter += Math.floor(rest / 60);
+        rest = rest % 60;
+    }
+    if (Math.floor(rest / 15) > 0) {
+        counter += Math.floor(rest / 15);
+        rest = rest % 15;
+    }
+    if (Math.floor(rest / 5) > 0) {
+        counter += Math.floor(rest / 5);
+        rest = rest % 5;
+    }
+
+    return counter + rest;
 };
+
+function getMinutes (value: string):number {
+    const arr = value.split(":");
+    return Number(arr[0])*60 + Number(arr[1]);
+}
