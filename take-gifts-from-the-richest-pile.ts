77a63a6583cf38811cdb8805b02be64331f1c2c9
d.ovsyannikova <dariannyko#gmail.com>
2024-08-19 23:06:09 +0300

    https://leetcode.com/problems/take-gifts-from-the-richest-pile

---
+++ take-gifts-from-the-richest-pile.ts
@@ -0,0 +1,16 @@
+function pickGifts(gifts: number[], k: number): number {
+  while (k) {
+    let maxInd = 0;
+    for (let i = 1; i < gifts.length; i++) {
+      if (gifts[i] > gifts[maxInd]) {
+        maxInd = i;
+      }
+    }
+    gifts[maxInd] = ~~Math.sqrt(gifts[maxInd]);
+    k--;
+  }
+
+  return gifts.reduce((sum, curr) => sum + curr, 0);
+}
+
+pickGifts([25, 64, 9, 4, 100], 4); // [5,8,9,4,3]
