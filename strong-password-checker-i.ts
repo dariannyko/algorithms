6f3a923b079020f4e25fce9f488e83d3c0d46f0f
d.ovsyannikova <dariannyko#gmail.com>
2024-10-05 17:23:43 +0300

    https://leetcode.com/problems/strong-password-checker-ii

--- strong-password-checker-i.ts
+++ strong-password-checker-i.ts
@@ -1,3 +1,27 @@
+const specialChars = new Set("!@#$%^&*()-+");
+
 function strongPasswordCheckerII(password: string): boolean {
-    // TODO
-};
+  const check = {
+    upper: false,
+    lower: false,
+    digit: false,
+    special: false,
+  };
+
+  if (password.length < 8) return false;
+
+  for (let i = 0; i < password.length; i++) {
+    const char = password[i];
+
+    if (i > 0 && char === password[i - 1]) return false;
+
+    if (char >= "a" && char <= "z") check.lower = true;
+    if (char >= "A" && char <= "Z") check.upper = true;
+    if (char >= "0" && char <= "9") check.digit = true;
+    if (specialChars.has(char)) check.special = true;
+  }
+
+  return Object.values(check).every((value) => value);
+}
+
+strongPasswordCheckerII("IloveLe3tcode!"); // true
