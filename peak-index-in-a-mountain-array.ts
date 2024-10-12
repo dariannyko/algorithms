8168aebc59d4cbe98be648fcc0d6871c540760bb
d.ovsyannikova <dariannyko#gmail.com>
2024-10-12 20:32:00 +0300

    https://leetcode.com/problems/peak-index-in-a-mountain-array

---
+++ peak-index-in-a-mountain-array.ts
@@ -0,0 +1,18 @@
+function peakIndexInMountainArray(arr: number[]): number {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    const middle = Math.floor((start + end) / 2);
+
+    if (arr[middle] < arr[middle + 1]) {
+      start = middle + 1;
+    } else {
+      end = middle;
+    }
+  }
+
+  return start;
+}
+
+peakIndexInMountainArray([0, 2, 1, 0]); // 1
