4046671a59c47996c3088fc2e4182ea2a711c329
dariannyko <dariannyko@gmail.com>
2024-10-20 21:18:32 +0300

    https://leetcode.com/problems/merge-sorted-array

---
+++ merge-sorted-array.ts
@@ -0,0 +1,22 @@
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  let a = m - 1;
+  let b = n - 1;
+  //   let c = n + m;
+  for (let i = nums1.length - 1; i >= 0; i--) {
+    if (nums1[a] > nums2[b]) {
+      nums1[i] = nums1[a];
+      a--;
+    }
+    if (nums1[a] < nums2[b]) {
+      nums1[i] = nums2[b];
+      b--;
+    }
+    if (nums1[a] === nums2[b]) {
+      nums1[i] = nums1[a];
+      a--;
+    }
+  }
+
+}
+
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
