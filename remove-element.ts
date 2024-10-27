7d437652d2b1ee74b487b6e695efa1c7acdab6fc
dariannyko <dariannyko@gmail.com>
2024-10-27 23:24:41 +0300

    https://leetcode.com/problems/remove-elemen

--- remove-element.ts
+++ remove-element.ts
@@ -1,6 +1,11 @@
-// todo: refactor
 function removeElement(nums: number[], val: number): number {
-  let j = 0;
-  for (let n of nums) if (n !== val) nums[j++] = n;
-  return j;
+  let ind = 0;
+
+  for (const num of nums) {
+    if (num !== val) nums[ind++] = num;
+  }
+
+  return ind;
 }
+
+removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2); // [0,1,3,0,4,_,_,_]
