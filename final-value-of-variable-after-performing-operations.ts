c4bdf40e8b5f4cc18961653d033653822bea6514
dariannyko <dariannyko@gmail.com>
2024-11-08 17:47:56 +0300

    refactor

--- final-value-of-variable-after-performing-operations.ts
+++ final-value-of-variable-after-performing-operations.ts
@@ -2,7 +2,7 @@ function finalValueAfterOperations(operations: string[]): number {
   let count = 0;
 
   for (let operation of operations) {
-    if (operation[1] === "+") {
+    if (operation[1] === "-") {
       count--;
     } else {
       count++;
