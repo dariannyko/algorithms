80045f45133bc4500587a0a52ddfab9ca04fe7e1
dariannyko <dariannyko@gmail.com>
2024-11-21 18:39:50 +0300

    https://leetcode.com/problems/contains-duplicate-ii

--- contains-duplicate-ii.ts
+++ contains-duplicate-ii.ts
@@ -1,14 +1,13 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
+  const map = new Map();
 
-    const mapArr = new Map();
-    
-    for(let i = 0;  i < nums.length;i++){
+  for (let i = 0; i < nums.length; i++) {
+    const index = map.get(nums[i]);
+    if (index >= 0 && Math.abs(i - index) <= k) return true;
+    map.set(nums[i], i);
+  }
 
-        let count  = mapArr.get(nums[i]);
-        if(count >= 0  && Math.abs( count - i) <= k){
-            return true;
-        }
-        mapArr.set(nums[i] , i);
-    }
-    return false
-};
+  return false;
+}
+
+containsNearbyDuplicate([1, 2, 3, 1], 3); // true
