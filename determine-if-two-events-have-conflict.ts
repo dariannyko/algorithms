23695b6f312276f8a0c7b2455f12191e9d6f0d20
dariannyko <dariannyko@gmail.com>
2024-12-08 21:25:08 +0300

    https://leetcode.com/problems/determine-if-two-events-have-conflict

--- determine-if-two-events-have-conflict.ts
+++ determine-if-two-events-have-conflict.ts
@@ -1,3 +1,6 @@
 function haveConflict(event1: string[], event2: string[]): boolean {
-    // TODO
+  return event2[0] <= event1[1] && event2[1] >= event1[0];
 }
+
+haveConflict(["01:15", "02:00"], ["02:00", "03:00"]); // true
+haveConflict(["16:53", "19:00"], ["10:33", "18:15"]); // true
