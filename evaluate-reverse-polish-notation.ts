ceda54e3e2ca728cb02f2471f0645fbc7dfaa809
dariannyko <dariannyko@gmail.com>
2024-12-16 22:24:26 +0300

    https://leetcode.com/problems/evaluate-reverse-polish-notation

--- evaluate-reverse-polish-notation.ts
+++ evaluate-reverse-polish-notation.ts
@@ -1,4 +1,28 @@
-// TODO
+const operations = {
+  "+": (a, b) => b + a,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => (a / b) | 0,
+};
+
 function evalRPN(tokens: string[]): number {
-  return 9;
+  const stack: number[] = [];
+
+  for (const token of tokens) {
+    if (!operations[token]) {
+      stack.push(+token);
+    } else {
+      const b = stack.pop();
+      const a = stack.pop();
+
+      const res = operations[token](a, b);
+      stack.push(res);
+    }
+  }
+
+  return stack[0];
 }
+
+// evalRPN(["2", "1", "+", "3", "*"]); // 9
+// evalRPN(["4", "13", "5", "/", "+"]); // 6
+evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]); // 22
