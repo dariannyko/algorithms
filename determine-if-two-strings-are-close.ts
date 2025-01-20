c2124ce22ff3b5f5ee423390ad286b8276a01855
dariannyko <dariannyko@gmail.com>
2025-01-20 22:26:17 +0300

    https://leetcode.com/problems/determine-if-two-strings-are-close

--- determine-if-two-strings-are-close.ts
+++ determine-if-two-strings-are-close.ts
@@ -1,26 +1,44 @@
-// TODO: solve using DP
+// function closeStrings(word1: string, word2: string): boolean {
+//   if (word1.length !== word2.length) return false;
+
+//   const map1 = new Map();
+//   const map2 = new Map();
+
+//   for (let i = 0; i < word1.length; i++) {
+//     map1.set(word1[i], (map1.get(word1[i]) || 0) + 1);
+//     map2.set(word2[i], (map2.get(word2[i]) || 0) + 1);
+//   }
+
+//   const arr1: string[] = [];
+//   const arr2: string[] = [];
+
+//   for (const [key, value] of map1) {
+//     if (!map2.has(key)) return false;
+
+//     arr1.push(value);
+//     arr2.push(map2.get(key));
+//   }
+
+//   return arr1.sort().toString() === arr2.sort().toString();
+// }
+
 function closeStrings(word1: string, word2: string): boolean {
   if (word1.length !== word2.length) return false;
 
-  const map1 = new Map();
-  const map2 = new Map();
+  const frequency1 = new Array(26).fill(0);
+  const frequency2 = new Array(26).fill(0);
 
   for (let i = 0; i < word1.length; i++) {
-    map1.set(word1[i], (map1.get(word1[i]) || 0) + 1);
-    map2.set(word2[i], (map2.get(word2[i]) || 0) + 1);
+    frequency1[word1.charCodeAt(i) - 97]++;
+    frequency2[word2.charCodeAt(i) - 97]++;
   }
 
-  const arr1: string[] = [];
-  const arr2: string[] = [];
-
-  for (const [key, value] of map1) {
-    if (!map2.has(key)) return false;
-
-    arr1.push(value);
-    arr2.push(map2.get(key));
+  for (let i = 0; i < frequency1.length; i++) {
+    if ((frequency1[i] === 0) !== (frequency2[i] === 0)) return false;
   }
 
-  return arr1.sort().toString() === arr2.sort().toString();
+  return frequency1.sort().toString() === frequency2.sort().toString();
 }
 
-closeStrings("cabbba", "abbccc"); // true
+// closeStrings("cabbba", "abbccc"); // true
+console.log(closeStrings("xxxxxxxxxxxxxxxxxxx", "zzzzzzzzzzzzzzzzzzz")); /// false
