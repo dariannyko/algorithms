df88c79c7b173138520b3519ef02aebd1539787d
dariannyko <dariannyko@gmail.com>
2025-02-25 18:33:43 +0300

    refactor

--- number-of-sub-arrays-with-odd-sum.ts
+++
@@ -1,9 +0,0 @@
-function numOfSubarrays(arr: number[]): number {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      console.log(i, j);
-    }
-  }
-}
-
-numOfSubarrays([1, 3, 5]);
