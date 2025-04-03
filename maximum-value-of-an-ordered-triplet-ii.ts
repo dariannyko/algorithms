004f38c80168a37593421d48c34b066326084a33
dariannyko <dariannyko@gmail.com>
2025-04-03 19:26:47 +0300

    https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii

---
+++ maximum-value-of-an-ordered-triplet-ii.ts
@@ -0,0 +1,16 @@
+function maximumTripletValue(nums: number[]): number {
+    let max = 0;
+    let maxDiff = 0;
+    let maxTriplet = 0;
+  
+    for (const num of nums) {
+      maxTriplet = Math.max(maxTriplet, num * maxDiff);
+      maxDiff = Math.max(maxDiff, max - num);
+      max = Math.max(max, num);
+    }
+  
+    return maxTriplet;
+  }
+  
+  maximumTripletValue([12, 6, 1, 2, 7]);
+  
