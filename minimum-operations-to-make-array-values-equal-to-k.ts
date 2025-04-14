c1952be9cbb7667bd926dc445034b2e9b6170ac4
dariannyko <dariannyko@gmail.com>
2025-04-14 22:32:35 +0300

    https://leetcode.com/problems/minimum-operations-to-make-array-values-equal-to-k

--- minimum-operations-to-make-array-values-equal-to-k.ts
+++ minimum-operations-to-make-array-values-equal-to-k.ts
@@ -1,9 +1,11 @@
-// TODO: refactor
 function minOperations(nums: number[], k: number): number {
-  const seen = new Set<number>();
+  const set = new Set();
   for (const num of nums) {
     if (num < k) return -1;
-    if (num > k) seen.add(num);
+    if (num > k) set.add(num);
   }
-  return seen.size;
+
+  return set.size;
 }
+
+minOperations([5, 2, 5, 4, 5], 2); // 2
