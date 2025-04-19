6a86e26683cb86dbb7403476d44d8023c0368279
dariannyko <dariannyko@gmail.com>
2025-04-19 16:41:45 +0300

    https://leetcode.com/problems/array-wrapper

--- array-wrapper.ts
+++ array-wrapper.ts
@@ -1,19 +1,20 @@
-// TODO: refactor
 class ArrayWrapper {
-    private nums: number[];
-  
-    constructor(nums: number[]) {
-      this.nums = nums;
-    }
-  
-    valueOf(): number {
-      return this.nums.reduce((acc, curr) => acc + curr, 0);
-    }
-  
-    toString(): string {
-      return JSON.stringify(this.nums);
-    }
+  private nums: number[];
+
+  constructor(nums: number[]) {
+    this.nums = nums;
   }
-  
-  const obj1 = new ArrayWrapper([1, 2]);
-  const obj2 = new ArrayWrapper([3, 4]);
+
+  valueOf(): number {
+    return this.nums.reduce((prev, curr) => prev + curr, 0);
+  }
+
+  toString(): string {
+    return JSON.stringify(this.nums);
+  }
+}
+
+const obj1 = new ArrayWrapper([1, 2]);
+const obj2 = new ArrayWrapper([3, 4]);
+obj1 + obj2; // 10
+ 
