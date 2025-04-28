909016c810f544667eac8a7a939b162e6b0f7d76
dariannyko <dariannyko@gmail.com>
2025-04-28 22:33:20 +0300

    https://leetcode.com/problems/merge-two-sorted-lists

--- merge-two-sorted-lists.ts
+++ merge-two-sorted-lists.ts
@@ -1,13 +1,18 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
 function mergeTwoLists(
   l1: ListNode | null,
   l2: ListNode | null
 ): ListNode | null {
-  if (l1 === null) {
-    return l2;
-  }
-  if (l2 === null) {
-    return l1;
-  }
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
 
   if (l1.val < l2.val) {
     l1.next = mergeTwoLists(l1.next, l2);
@@ -17,3 +22,8 @@ function mergeTwoLists(
     return l2;
   }
 }
+
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+mergeTwoLists(list1, list2);
