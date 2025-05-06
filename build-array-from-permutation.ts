0a524ddbd7b80a1ae023b2d0378466cb79f6eb40
dariannyko <dariannyko@gmail.com>
2025-05-06 18:30:30 +0300

    https://leetcode.com/problems/build-array-from-permutation

--- build-array-from-permutation.ts
+++ build-array-from-permutation.ts
@@ -1,10 +1,5 @@
 function buildArray(nums: number[]): number[] {
-  const res: number[] = [];
-
-  for (let num of nums) {
-    res.push(nums[num]);
-  }
-  return res;
+  return nums.map((v) => nums[v]);
 }
 
 buildArray([0, 2, 1, 5, 3, 4]); // [0,1,2,4,5,3]
