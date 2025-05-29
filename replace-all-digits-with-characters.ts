0805f161f4f0bf26170cf03acc60d03dc5c5f702
dariannyko <dariannyko@gmail.com>
2025-05-29 19:21:00 +0300

    https://leetcode.com/problems/replace-all-digits-with-characters

---
+++ replace-all-digits-with-characters.ts
@@ -0,0 +1,20 @@
+function replaceDigits(s: string): string {
+  let res = "";
+
+  for (let i = 0; i < s.length; i += 2) {
+    const char = s[i];
+    const step =
+      s[i + 1] === undefined
+        ? ""
+        : String.fromCharCode(char.charCodeAt(0) + Number(s[i + 1]));
+
+    res += char + step;
+  }
+
+  return res;
+}
+
+replaceDigits("a1c1e1");
+replaceDigits("a1b2c3d4e");
+console.log(String.fromCharCode("a".charCodeAt(0) - 97 + +"1" + 97));
+// console.log('a'.charCodeAt(0)-97 + );
