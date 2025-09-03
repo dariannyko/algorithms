33476bf865fc0ab1ac7876cc631ecb10e4d888a7
Daria <dubi-daba@MacBook-Pro-Daria.local>
2025-09-03 22:40:46 +0300

    https://leetcode.com/problems/reverse-string

--- reverse-string.ts
+++ reverse-string.ts
@@ -2,17 +2,17 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-    if(s.length === 0) {
-        throw new Error('Constraints not met')
-    }
-    let leftPointer = 0;
-    let rightPointer = s.length - 1;
-    
-    while(leftPointer < rightPointer) {
-        const temp = s[leftPointer];
-        s[leftPointer] = s[rightPointer];
-        s[rightPointer] = temp;
-        leftPointer++;
-        rightPointer--;
-    }
-};
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    let leftChar = s[left];
+    s[left] = s[right];
+    s[right] = leftChar;
+
+    left++;
+    right--;
+  }
+}
+
+reverseString(["h", "e", "l", "l", "o"]); // [ 'o', 'l', 'l', 'e', 'h' ]
