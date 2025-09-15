2be44b1c578f8718a29435ce7ca08aed2e5a2fb7
Daria <dubi-daba@MacBook-Pro-Daria.local>
2025-09-15 23:29:39 +0300

    https://leetcode.com/problems/3sum

--- study/two-sum-ii-input-array-is-sorted.ts
+++ study/two-sum-ii-input-array-is-sorted.ts
@@ -1,3 +1,4 @@
+// #3
 function twoSum(numbers: number[], target: number): number[] {
   let start = 0;
   let end = numbers.length - 1;
@@ -5,12 +6,12 @@ function twoSum(numbers: number[], target: number): number[] {
   while (start < end) {
     const sum = numbers[start] + numbers[end];
 
-    if (sum === target) return [start + 1, end + 1];
-
-    if (sum < target) {
-      start++;
-    } else {
+    if (sum === target) {
+      return [start + 1, end + 1];
+    } else if (sum > target) {
       end--;
+    } else {
+      start++;
     }
   }
 }
