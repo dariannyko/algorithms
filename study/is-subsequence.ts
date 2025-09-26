2ae16494e5ad5de466afa9f4bf45c4a6bd5cc4c7
Daria <dubi-daba@MacBook-Pro-Daria.local>
2025-09-26 19:11:13 +0300

    https://leetcode.com/problems/valid-triangle-number

---
+++ study/is-subsequence.ts
@@ -0,0 +1,11 @@
+function isSubsequence(s: string, t: string): boolean {
+  let substrInd = 0;
+
+  for (let i = 0; i < t.length && substrInd < s.length; i++) {
+    if (s[substrInd] === t[i]) {
+      substrInd++;
+    }
+  }
+
+  return substrInd === s.length;
+}
