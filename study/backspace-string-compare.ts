a4c45baf54532efd055b74c00c9af7e9232d4480
Daria <dubi-daba@MacBook-Pro-Daria.local>
2025-09-30 23:42:50 +0300

    https://leetcode.com/problems/backspace-string-compare

--- study/backspace-string-compare.ts
+++ study/backspace-string-compare.ts
@@ -3,40 +3,41 @@ function backspaceCompare(s: string, t: string): boolean {
   let sPointer = s.length - 1;
   let tPointer = t.length - 1;
 
-  let sSkip = 0;
-  let tSkip = 0;
+  while (sPointer >= 0 || tPointer >= 0) {
+    while (sPointer >= 0) {
+      let sSkip = 0;
 
-  while (sPointer >= 0 && tPointer >= 0) {
-    if (s[sPointer] === "#") sSkip++;
-    if (t[tPointer] === "#") tSkip++;
-
-    console.log(sSkip);
-    if (s[sPointer] !== "#" && sSkip > 0) {
-      sPointer -= 1;
-      sSkip--;
-    }
-    console.log(tSkip);
-    if (t[tPointer] !== "#" && tSkip > 0) {
-      tPointer -= 1;
-      tSkip--;
+      if (s[sPointer] === "#") {
+        sSkip++;
+      } else if (sSkip > 0) {
+        sSkip--;
+      } else {
+        break;
+      }
+      sPointer--;
     }
 
-    // if (
-    //   s[sPointer] !== "#" &&
-    //   t[tPointer] !== "#" &&
-    //   s[sPointer] !== t[tPointer]
-    // )
-    //   return false;
-    console.log(t[tPointer]);
-    console.log(s[sPointer]);
+    while (tPointer >= 0) {
+      let tSkip = 0;
+
+      if (t[tPointer] === "#") {
+        tSkip++;
+      } else if (tSkip > 0) {
+        tSkip--;
+      } else {
+        break;
+      }
+      tPointer--;
+    }
+
+    if (s[sPointer] !== t[tPointer]) return false;
 
     sPointer--;
     tPointer--;
-    console.log(sPointer);
   }
-
-  console.log(sPointer);
-  console.log(tPointer);
+  return sPointer === tPointer;
 }
-                    
-backspaceCompare("ab##", "c#d#");
+
+// backspaceCompare("ab##", "c#d#");
+// backspaceCompare("a##c", "#a#c");
+// backspaceCompare("ab#c", "ad#c");
