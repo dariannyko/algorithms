67e26aa65935fe68f547387db8d407bdbcb7f367
Daria <dubi-daba@Mac.Dlink>
2025-10-29 12:47:21 +0300

    https://leetcode.com/problems/smallest-number-with-all-set-bits

--- smallest-number-with-all-set-bits.ts
+++ smallest-number-with-all-set-bits.ts
@@ -1,9 +1,11 @@
 function smallestNumber(n: number): number {
-    const binariNumber = n.toString(2);
-    const binaryLength = binariNumber.length
-    let setBit = ''
-    for (let i=0; i < binaryLength; i++) {
-        setBit += '1'
-    }
-    return parseInt(setBit, 2);
-};
+  const binariNumber = n.toString(2);
+  const binaryLength = binariNumber.length;
+  let setBit = "";
+  for (let i = 0; i < binaryLength; i++) {
+    setBit += "1";
+  }
+  return parseInt(setBit, 2);
+}
+
+smallestNumber(5); // 7
