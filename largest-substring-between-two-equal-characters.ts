b6e008cf3239e3710d4340c57a1f18aaf3987669
Daria <dubi-daba@MacBook-Pro-Daria.local>
2025-11-07 19:27:45 +0300

    https://leetcode.com/problems/largest-substring-between-two-equal-characters

---
+++ largest-substring-between-two-equal-characters.ts
@@ -0,0 +1,10 @@
+function maxLengthBetweenEqualCharacters(s: string) {
+  const map = new Map([[s[0], 0]]),
+    max = Math.max;
+  let res = -1;
+  for (let i = 1, n = s.length; i < n; i++) {
+    const item = map.get(s[i]);
+    item === undefined ? map.set(s[i], i) : (res = max(res, i - item - 1));
+  }
+  return res;
+}
