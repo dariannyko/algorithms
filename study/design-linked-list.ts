e991b287d26d7c83662920fe2ad546e813f3a586
dariannyko <d.ovsyannikova@bft.ru>
2025-12-01 22:36:27 +0300

    https://leetcode.com/problems/design-linked-list

--- study/design-linked-list.ts
+++ study/design-linked-list.ts
@@ -13,65 +13,25 @@ class MyLinkedList {
     this.size = 0;
   }
 
-  getNode(index: number): Node | null {
-    if (index < 0 || index >= this.size) return null;
-    let count = 0;
-    let node = this.head;
-    while (count < index) {
-      node = node.next;
-      count++;
-    }
-    return node;
-  }
+    get(index: number): number {
 
-  get(index: number): number {
-    const node = this.getNode(index);
-    if (!node) return -1;
-    return node.val;
-  }
-
-  addAtHead(val: number): void {
-    const next = this.head;
-    this.head = { val, next };
-    this.size++;
-  }
-
-  addAtTail(val: number): void {
-    if (this.size === 0) {
-      this.addAtHead(val);
-      return;
     }
-    const node = this.getNode(this.size - 1);
-    node.next = { val, next: null };
-    this.size++;
-  }
 
-  addAtIndex(index: number, val: number): void {
-    if (index < 0 || index > this.size) return;
-    if (index === this.size) {
-      this.addAtTail(val);
-      return;
+    addAtHead(val: number): void {
+        
     }
-    if (index === 0) {
-      this.addAtHead(val);
-      return;
-    }
-    const node = this.getNode(index - 1);
-    node.next = { val, next: node.next };
-    this.size++;
-  }
 
-  deleteAtIndex(index: number): void {
-    if (index < 0 || index >= this.size) return;
-    if (this.size === 0) return;
-    if (index === 0) {
-      this.head = this.head.next;
-      this.size--;
-      return;
+    addAtTail(val: number): void {
+        
     }
-    const node = this.getNode(index - 1);
-    node.next = node.next?.next || null;
-    this.size--;
+
+    addAtIndex(index: number, val: number): void {
+        
+    }
+
+    deleteAtIndex(index: number): void {
+        
+    }
   }
 }
 
