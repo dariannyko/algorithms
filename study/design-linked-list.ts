a1e2c07464228a920a59c13aa34aa725bb52381a
dariannyko <d.ovsyannikova@bft.ru>
2025-12-18 23:17:44 +0300

    https://leetcode.com/problems/sum-of-unique-elements

--- study/design-linked-list.ts
+++ study/design-linked-list.ts
@@ -12,69 +12,61 @@ class MyLinkedList {
     this.head = null;
     this.size = 0;
   }
-
-  getNode(index: number) {
-    if (index < 0 || index >= this.size) return null;
-
-    let node = this.head;
-    for (let i = 0; i < index; i++) node = node.next;
-
-    return node;
-  }
-
+  
   get(index: number): number {
-    const node = this.getNode(index);
-    return node ? node.val : -1;
+    if (index < 0 || index >= this.size) return -1;
+
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
+    }
+
+    return current.val;
   }
 
   addAtHead(val: number): void {
-    const next = this.head;
-    this.head = { val, next };
-    this.size++;
+    this.addAtIndex(0, val);
   }
 
   addAtTail(val: number): void {
-    if (this.size === 0) {
-      this.addAtHead(val);
-      return;
-    }
-
-    const node = this.getNode(this.size - 1);
-    if (node) {
-      node.next = { val, next: null };
-      this.size++;
-    }
+    this.addAtIndex(this.size, val);
   }
 
   addAtIndex(index: number, val: number): void {
     if (index < 0 || index > this.size) return;
 
+    this.size++;
+    let current = this.head;
+
     if (index === 0) {
-      this.addAtHead(val);
-      return;
-    }
-    if (index === this.size) {
-      this.addAtTail(val);
+      const node = { val, next: current };
+      this.head = node;
       return;
     }
 
-    let node = this.getNode(index - 1);
-    if (node) {
-      node.next = { val, next: node.next };
-      this.size++;
+    for (let i = 0; i < index - 1; i++) {
+      current = current.next;
     }
+
+    const oldCurrent = current.next;
+
+    current.next = { val, next: oldCurrent };
   }
 
   deleteAtIndex(index: number): void {
-    if (index < 0 || index >= this.size || this.size === 0) return;
+    if (index < 0 || index >= this.size) return;
+    this.size--;
+
+    let current = this.head;
     if (index === 0) {
-      this.head = this.head.next;
-      this.size--;
+      this.head = current.next;
       return;
     }
-    const node = this.getNode(index - 1);
-    if (node) node.next = node?.next?.next || null;
-    this.size--;
+
+    for (let i = 0; i < index - 1; i++) {
+      current = current.next;
+    }
+    current.next = current.next.next;
   }
 }
 
