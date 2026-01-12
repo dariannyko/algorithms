c99a03f23ac7dea857c85446050f49491f9970e3
dariannyko <d.ovsyannikova@bft.ru>
2026-01-12 21:59:24 +0300

    https://leetcode.com/problems/minimum-time-visiting-all-points

--- minimum-time-visiting-all-points.ts
+++ minimum-time-visiting-all-points.ts
@@ -4,7 +4,6 @@ function minTimeToVisitAllPoints(points: number[][]): number {
   for (let i = 0; i < points.length - 1; i++) {
     const x = Math.abs(points[i + 1][0] - points[i][0]);
     const y = Math.abs(points[i + 1][1] - points[i][1]);
-
     res += Math.max(x, y);
   }
 
