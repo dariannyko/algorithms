fbf604c13837ecdc3ad71668c8ea1a0ec8e4335e
dariannyko <d.ovsyannikova@bft.ru>
2026-01-15 22:37:21 +0300

    https://leetcode.com/problems/calculate-amount-paid-in-taxes

--- calculate-amount-paid-in-taxes.ts
+++ calculate-amount-paid-in-taxes.ts
@@ -1,1 +1,10 @@
-function calculateTax(brackets: number[][], income: number): number {}
+function calculateTax(brackets: number[][], income: number): number {
+  let out = 0;
+  for (let i = 0; i < brackets.length; i++) {
+    let p = Math.min(income, brackets[i][0]);
+    if (i) p -= brackets[i - 1][0];
+    out += (p * brackets[i][1]) / 100;
+    if (brackets[i][0] >= income) break;
+  }
+  return out;
+}
