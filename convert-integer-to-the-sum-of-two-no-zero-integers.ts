cdd79c0e25107ffc4a2ee66d3be30a67854d766b
dariannyko <d.ovsyannikova@bft.ru>
2026-01-19 23:36:44 +0300

    https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target

--- convert-integer-to-the-sum-of-two-no-zero-integers.ts
+++ convert-integer-to-the-sum-of-two-no-zero-integers.ts
@@ -1,13 +1,3 @@
-// const containZeroes = (number: number) => {
-//   while (number > 0) {
-//     const num = number % 10;
-//     if (num === 0) return true;
-
-//     number = Math.floor(number / 10);
-//   }
-//   return false;
-// };
-
 function getNoZeroIntegers(n: number): number[] {
   for (let i = 1; i < n; i++) {
     if ((n - i).toString().includes("0") || i.toString().includes("0"))
