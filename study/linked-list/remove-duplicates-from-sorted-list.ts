f5d1f8cd4d06391683fe984e677c68d2bd2a8a29
dariannyko <d.ovsyannikova@bft.ru>
2026-01-28 23:39:55 +0300

    https://leetcode.com/problems/remove-duplicates-from-sorted-list

--- study/linked-list/remove-duplicates-from-sorted-list.ts
+++ study/linked-list/remove-duplicates-from-sorted-list.ts
@@ -1,13 +1,23 @@
-function deleteDuplicates(head: ListNode | null): ListNode | null {
-  let node = head;
-  while (node !== null) {
-    if (node.next !== null) {
-      if (node.val === node.next.val) {
-        node.next = node.next.next;
-        continue;
-      }
-    }
-    node = node.next;
+//  Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
   }
+}
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  let current = head;
+
+  while (current && current.next) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
   return head;
 }
