6100ca96ded4cfddea73e7e5805420aed6d5a7c9
dariannyko <d.ovsyannikova@bft.ru>
2026-02-05 23:45:38 +0300

    https://leetcode.com/problems/transformed-array

---
+++ transformed-array.ts
@@ -0,0 +1,14 @@
+function constructTransformedArray(nums: number[]): number[] {
+  const n = nums.length;
+  const result = Array(n);
+  for (let i = 0; i < n; i++) {
+    if (nums[i] > 0) {
+      result[i] = nums[(i + nums[i]) % n];
+    } else if (nums[i] === 0) {
+      result[i] = nums[i];
+    } else if (nums[i] < 0) {
+      result[i] = nums[(((i + nums[i]) % n) + n) % n];
+    }
+  }
+  return result;
+}
