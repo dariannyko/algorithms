d8dd7dfd089375eab378cea8d0f1d4b836daf5f5
dariannyko <d.ovsyannikova@bft.ru>
2026-02-21 16:51:24 +0300

    https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation

---
+++ prime-number-of-set-bits-in-binary-representation.ts
@@ -0,0 +1,23 @@
+const primeNumbers = new Set([2, 3, 5, 7, 11, 13, 17, 19]);
+
+function numberOfSetBits(num: number) {
+  const binary = num.toString(2);
+
+  let setBits = 0;
+
+  for (let i = 0; i < binary.length; i++) {
+    if (Number(binary[i])) setBits++;
+  }
+
+  return setBits;
+}
+
+function countPrimeSetBits(left: number, right: number): number {
+  let count = 0;
+
+  for (let i = left; i <= right; i++) {
+    if (primeNumbers.has(numberOfSetBits(i))) count++;
+  }
+
+  return count;
+}
