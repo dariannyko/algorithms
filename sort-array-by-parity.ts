2ac27a7d33d218f1d0772cc7d2638fd4d40156b3
dariannyko <d.ovsyannikova@bft.ru>
2026-02-24 23:42:49 +0300

    https://leetcode.com/problems/sort-array-by-parity

--- sort-array-by-parity.ts
+++ sort-array-by-parity.ts
@@ -1,22 +1,16 @@
 function sortArrayByParity(nums: number[]): number[] {
-  let i = 0;
-  let j = nums.length - 1;
-  let k = 0;
+  let left = 0;
 
-  while (k <= j) {
-    if (nums[k] % 2 === 0) {
-      let temp = nums[i];
-      nums[i] = nums[k];
-      nums[k] = temp;
-      i++;
-      k++;
-    } else {
-      let temp = nums[j];
-      nums[j] = nums[k];
-      nums[k] = temp;
-      j--;
+  for (let right = 0; right < nums.length; right++) {
+    if (nums[right] % 2 === 0) {
+      const temp = nums[left];
+      nums[left] = nums[right];
+      nums[right] = temp;
+      left++;
     }
   }
 
   return nums;
 }
+
+sortArrayByParity([3, 1, 2, 4]);
