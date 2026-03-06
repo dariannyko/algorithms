3ef8dfcead41579db21f3e222f2b2ec341698e9f
dariannyko <d.ovsyannikova@bft.ru>
2026-03-06 22:19:30 +0300

    https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones

---
+++ study/check-if-binary-string-has-at-most-one-segment-of-ones.ts
@@ -0,0 +1,8 @@
+function checkOnesSegment(s: string): boolean {
+  let end = false;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "0") end = true;
+    else if (s[i] === "1" && end) return false;
+  }
+  return true;
+}
