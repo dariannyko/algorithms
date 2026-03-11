efdddb4c9b74dbdc5b9d4ecfd8dbe894eb453e65
dariannyko <d.ovsyannikova@bft.ru>
2026-03-11 23:49:19 +0300

    https://leetcode.com/problems/absolute-difference-between-maximum-and-minimum-k-elements

--- absolute-difference-between-maximum-and-minimum-k-elements.ts
+++ absolute-difference-between-maximum-and-minimum-k-elements.ts
@@ -1,5 +1,9 @@
-const absDifference = (nums: number[], k: number) =>
-  nums
-    .sort((a, b) => a - b)
-    .slice(-k)
-    .reduce((a, b) => a + b) - nums.slice(0, k).reduce((a, b) => a + b);
+const absDifference = (nums: number[], k: number) => {
+  nums.sort((a, b) => a - b);
+  const b = nums.slice(0, k).reduce((curr, sum) => curr + sum, 0);
+  const a = nums.slice(nums.length - k).reduce((curr, sum) => curr + sum, 0);
+
+  return a - b;
+};
+
+absDifference([5, 2, 2, 4], 2); // 5
