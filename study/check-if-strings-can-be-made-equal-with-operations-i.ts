4eb159474b1716eb726e97a98fa0ad78dbf5f4b5
dariannyko <d.ovsyannikova@bft.ru>
2026-03-29 22:09:03 +0300

    https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i

---
+++ study/check-if-strings-can-be-made-equal-with-operations-i.ts
@@ -0,0 +1,9 @@
+function canBeEqual(s1: string, s2: string) {
+  const a1 = s1.split("");
+  for (let i = 0; i < 2; i += 1) {
+    if (a1[i] !== s2[i]) {
+      [a1[i], a1[i + 2]] = [a1[i + 2], a1[i]];
+    }
+  }
+  return a1.every((c, i) => c === s2[i]);
+}
