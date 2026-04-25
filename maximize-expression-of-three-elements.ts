999b22d4b3cfc79e3de6f4b954b4d5f93895d698
dariannyko <d.ovsyannikova@bft.ru>
2026-04-25 15:58:33 +0300

    https://leetcode.com/problems/maximize-expression-of-three-elements

---
+++ maximize-expression-of-three-elements.ts
@@ -0,0 +1,11 @@
+function maximizeExpressionOfThree(nums: number[]) {
+  let mn = Infinity,
+    mx1 = -Infinity,
+    mx2 = mx1;
+  for (const num of nums) {
+    if (num < mn) mn = num;
+    if (num >= mx2) [mx1, mx2] = [mx2, num];
+    else if (num > mx1) mx1 = num;
+  }
+  return mx1 + mx2 - mn;
+}
