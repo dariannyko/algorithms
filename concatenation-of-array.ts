b4c3c69ed360ac588647df34abe508baee7b8bbe
dariannyko <d.ovsyannikova@bft.ru>
2026-05-04 22:31:24 +0300

    https://leetcode.com/problems/concatenation-of-array

--- concatenation-of-array.ts
+++ concatenation-of-array.ts
@@ -1,4 +1,10 @@
 function getConcatenation(nums: number[]): number[] {
-  return nums.concat(nums);
+  const arr = new Array(nums.length * 2);
+
+  for (let i = 0; i < nums.length; i++) {
+    arr[i] = nums[i];
+    arr[i + nums.length] = nums[i];
+  }
+  return arr;
 }
 getConcatenation([1, 3, 2, 1]);
