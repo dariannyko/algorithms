cf4557bf41e0ba12aba0afdd04d264d2542c74f6
dariannyko <d.ovsyannikova@bft.ru>
2026-05-10 23:03:36 +0300

    https://leetcode.com/problems/minimum-depth-of-binary-tree

--- study/tree/minimum-depth-of-binary-tree.ts
+++ study/tree/minimum-depth-of-binary-tree.ts
@@ -1,11 +1,33 @@
+// function minDepth(root: TreeNode | null): number {
+//   if (root === null) return 0;
+
+//   const left = minDepth(root.left) + 1;
+//   const right = minDepth(root.right) + 1;
+
+//   if (!root.left) return right;
+//   if (!root.right) return left;
+
+//   return Math.min(left, right);
+// }
+
 function minDepth(root: TreeNode | null): number {
-  if (root === null) return 0;
+  if (!root) return 0;
 
-  const left = minDepth(root.left) + 1;
-  const right = minDepth(root.right) + 1;
+  let stack = [root];
+  let level = 1;
 
-  if (!root.left) return right;
-  if (!root.right) return left;
+  while (stack.length > 0) {
+    const newStack = [];
+    while (stack.length > 0) {
+      const node = stack.pop();
+      if (!(node.left || node.right)) return level;
+      if (node.left) newStack.push(node.left);
+      if (node.right) newStack.push(node.right);
+    }
 
-  return Math.min(left, right);
+    stack = newStack;
+    level++;
+  }
+
+  return level;
 }
