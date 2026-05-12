66c8f61a407b4558b0cb0f597af17cf207a2b738
dariannyko <d.ovsyannikova@bft.ru>
2026-05-12 23:25:14 +0300

    https://leetcode.com/problems/minimum-depth-of-binary-tree

--- study/tree/minimum-depth-of-binary-tree.ts
+++ study/tree/minimum-depth-of-binary-tree.ts
@@ -1,33 +1,62 @@
-// function minDepth(root: TreeNode | null): number {
-//   if (root === null) return 0;
-
-//   const left = minDepth(root.left) + 1;
-//   const right = minDepth(root.right) + 1;
-
-//   if (!root.left) return right;
-//   if (!root.right) return left;
-
-//   return Math.min(left, right);
-// }
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
 
 function minDepth(root: TreeNode | null): number {
   if (!root) return 0;
 
-  let stack = [root];
-  let level = 1;
+  const left = minDepth(root.left);
+  const right = minDepth(root.right);
 
-  while (stack.length > 0) {
-    const newStack = [];
-    while (stack.length > 0) {
-      const node = stack.pop();
-      if (!(node.left || node.right)) return level;
-      if (node.left) newStack.push(node.left);
-      if (node.right) newStack.push(node.right);
-    }
+  if (!root.left) return right + 1;
+  if (!root.right) return left + 1;
 
-    stack = newStack;
-    level++;
-  }
-
-  return level;
+  return Math.min(left, right) + 1;
 }
+
+// function minDepth(root: TreeNode | null): number {
+//   if (!root) return 0;
+
+//   let stack = [root];
+//   let level = 1;
+
+//   while (stack.length > 0) {
+//     const newStack = [];
+//     while (stack.length > 0) {
+//       const node = stack.pop();
+//       if (!(node.left || node.right)) return level;
+//       if (node.left) newStack.push(node.left);
+//       if (node.right) newStack.push(node.right);
+//     }
+
+//     stack = newStack;
+//     level++;
+//   }
+
+//   return level;
+// }
+
+// const tree = new TreeNode(
+//   3,
+//   new TreeNode(9),
+//   new TreeNode(20, new TreeNode(15, new TreeNode(1)), new TreeNode(7)),
+// );
+
+const tree = new TreeNode(
+  2,
+  null,
+  new TreeNode(
+    3,
+    null,
+    new TreeNode(4, null, new TreeNode(5, null, new TreeNode(6))),
+  ),
+);
+minDepth(tree); // 5
