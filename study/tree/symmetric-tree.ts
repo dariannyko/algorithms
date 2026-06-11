560ca3481a3faf186ee837ad60028e08423fd92f
dariannyko <d.ovsyannikova@bft.ru>
2026-06-11 23:30:52 +0300

    https://leetcode.com/problems/symmetric-tree/

--- study/tree/symmetric-tree.ts
+++ study/tree/symmetric-tree.ts
@@ -8,25 +8,41 @@ class TreeNode {
     this.right = right === undefined ? null : right;
   }
 }
+// function isSymmetric(root: TreeNode | null): boolean {
+//   if (!root) return true;
+
+//   const stack = [root.left, root.right];
+
+//   while (stack.length > 0) {
+//     const right = stack.pop();
+//     const left = stack.pop();
+
+//     if (!right && !left) continue;
+//     if (!right || !left) return false;
+//     if (right.val !== left.val) return false;
+
+//     stack.push(left.left);
+//     stack.push(right.right);
+//     stack.push(left.right);
+//     stack.push(right.left);
+//   }
+//   return true;
+// }
+
+const isMirror = (tree1: TreeNode | null, tree2: TreeNode | null): boolean => {
+  if (!tree1 && !tree2) return true;
+  if (!tree1 || !tree2) return false;
+  return (
+    tree1.val === tree2.val &&
+    isMirror(tree1.left, tree2.right) &&
+    isMirror(tree1.right, tree2.left)
+  );
+};
+
 function isSymmetric(root: TreeNode | null): boolean {
   if (!root) return true;
 
-  const stack = [root.left, root.right];
-
-  while (stack.length > 0) {
-    const right = stack.pop();
-    const left = stack.pop();
-
-    if (!right && !left) continue;
-    if (!right || !left) return false;
-    if (right.val !== left.val) return false;
-
-    stack.push(left.left);
-    stack.push(right.right);
-    stack.push(left.right);
-    stack.push(right.left);
-  }
-  return true;
+  return isMirror(root.left, root.right);
 }
 
 const tree = new TreeNode(
@@ -34,5 +50,4 @@ const tree = new TreeNode(
   new TreeNode(2, new TreeNode(3), new TreeNode(4)),
   new TreeNode(2, new TreeNode(4), new TreeNode(3)),
 );
-
 isSymmetric(tree);
