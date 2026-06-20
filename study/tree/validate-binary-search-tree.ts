8ceb5f24391fae95c34e35466a4f6aeaa89046f0
dariannyko <d.ovsyannikova@bft.ru>
2026-06-20 17:27:52 +0300

    feat/039146893

--- study/tree/validate-binary-search-tree.ts
+++ study/tree/validate-binary-search-tree.ts
@@ -1,14 +1,14 @@
 //  Definition for a binary tree node.
-class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
-}
+// class TreeNode {
+//   val: number;
+//   left: TreeNode | null;
+//   right: TreeNode | null;
+//   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//     this.val = val === undefined ? 0 : val;
+//     this.left = left === undefined ? null : left;
+//     this.right = right === undefined ? null : right;
+//   }
+// }
 
 function isValidBST(root: TreeNode | null): boolean {
   if (!root) return false;
@@ -21,7 +21,7 @@ function isValidBST(root: TreeNode | null): boolean {
     if (!node) continue;
 
     if (node.val <= minRange || node.val >= maxRange) return false;
-    
+
     stack.push([node.left, minRange, node.val]);
     stack.push([node.right, node.val, maxRange]);
   }
