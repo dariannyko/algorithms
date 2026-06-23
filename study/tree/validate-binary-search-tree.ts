51c8d20c039bcaeed01d1060c479813eee440e9b
dariannyko <d.ovsyannikova@bft.ru>
2026-06-23 23:13:11 +0300

    https://leetcode.com/problems/validate-binary-search-tree

--- study/tree/validate-binary-search-tree.ts
+++ study/tree/validate-binary-search-tree.ts
@@ -1,46 +1,42 @@
 //  Definition for a binary tree node.
-// class TreeNode {
-//   val: number;
-//   left: TreeNode | null;
-//   right: TreeNode | null;
-//   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-//     this.val = val === undefined ? 0 : val;
-//     this.left = left === undefined ? null : left;
-//     this.right = right === undefined ? null : right;
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+// function isValidBST(root: TreeNode | null): boolean {
+//   if (!root) return false;
+
+//   const stack: [TreeNode, number, number][] = [[root, -Infinity, Infinity]];
+
+//   while (stack.length) {
+//     const [node, minRange, maxRange] = stack.pop()!;
+
+//     if (!node) continue;
+
+//     if (node.val <= minRange || node.val >= maxRange) return false;
+
+//     stack.push([node.left, minRange, node.val]);
+//     stack.push([node.right, node.val, maxRange]);
 //   }
+
+//   return true;
 // }
 
-function isValidBST(root: TreeNode | null): boolean {
-  if (!root) return false;
-
-  const stack: [TreeNode, number, number][] = [[root, -Infinity, Infinity]];
-
-  while (stack.length) {
-    const [node, minRange, maxRange] = stack.pop()!;
-
-    if (!node) continue;
-
-    if (node.val <= minRange || node.val >= maxRange) return false;
-
-    stack.push([node.left, minRange, node.val]);
-    stack.push([node.right, node.val, maxRange]);
-  }
-
-  return true;
-}
-
 function validate(root: TreeNode | null, min: number, max: number): boolean {
-  if (!root) {
-    return true;
-  }
+  if (!root) return true;
 
-  if (root.val >= max || root.val <= min) {
-    return false;
-  }
+  if (root.val <= min || root.val >= max) return false;
 
-  return (
-    validate(root.left, min, root.val) && validate(root.right, root.val, max)
-  );
+  const left = validate(root.left, min, root.val);
+  const right = validate(root.right, root.val, max);
+
+  return left && right;
 }
 
 function isValidBST(root: TreeNode | null): boolean {
