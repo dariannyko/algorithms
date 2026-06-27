e5c113b620102d8e445e8bce71305c4b7aca4521
dariannyko <d.ovsyannikova@bft.ru>
2026-06-27 16:23:19 +0300

    https://leetcode.com/problems/increasing-order-search-tree

--- study/tree/increasing-order-search-tree.ts
+++ study/tree/increasing-order-search-tree.ts
@@ -1,14 +1,3 @@
-class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
-}
-
 function increasingBST(root: TreeNode | null): TreeNode | null {
   const dummy = new TreeNode(0);
   let current = dummy;
