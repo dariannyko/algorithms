5ac752ddcc029c477a5505583c68f7378703f71a
dariannyko <d.ovsyannikova@bft.ru>
2026-07-11 17:48:57 +0300

    https://leetcode.com/problems/count-complete-tree-nodes

--- study/tree/count-complete-tree-nodes/description.ts
+++ study/tree/count-complete-tree-nodes/description.ts
@@ -1,3 +1,15 @@
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
 function countNodes(root: TreeNode | null, count: number = 0): number {
   return root
     ? 1 + countNodes(root.left, count) + countNodes(root.right, count)
