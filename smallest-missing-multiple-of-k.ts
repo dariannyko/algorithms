a1adf557ad56c5f14d15eafff031cde6e4acf293
dariannyko <d.ovsyannikova@bft.ru>
2026-08-25 23:22:35 +0300

    https://leetcode.com/problems/smallest-missing-multiple-of-k

---
+++ smallest-missing-multiple-of-k.ts
@@ -0,0 +1,13 @@
+function missingMultiple(nums: number[], k: number): number {
+  const hashSet: Set<number> = new Set(nums);
+
+  let i: number = 1;
+
+  while (true) {
+    if (!hashSet.has(i * k)) {
+      return i * k;
+    }
+
+    i++;
+  }
+}
